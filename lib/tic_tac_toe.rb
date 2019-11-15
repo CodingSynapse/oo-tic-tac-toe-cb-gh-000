b35cf21c37aa5e181245d444eaa69784b812cf18
Micah ortega <codingsynapse@gmail.com>
2019-11-15 00:56:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,17 +1,18 @@
 class TicTacToe
+  WIN_COMBINATIONS = [
+      [0, 1, 2], # Top row
+      [3, 4, 5], # middle row
+      [6, 7, 8], #bottom row
+      [0, 3, 6], #left columns
+      [1, 4, 7], #middle columns
+      [2 ,5, 8], #right columns
+      [2, 4, 6], #diagonal
+      [0, 4, 8]  #diagonal
+    ]
 
   def initialize
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
-    def WIN_COMBINATIONS = [
-    [0, 1, 2], # Top row
-    [3, 4, 5], # middle row
-    [6, 7, 8], #bottom row
-    [0, 3, 6], #left columns
-    [1, 4, 7], #middle columns
-    [2 ,5, 8], #right columns
-    [2, 4, 6], #diagonal
-    [0, 4, 8]  #diagonal
-  ]
+
 
 end
