9be6fd466a2d503a469826f9263b8187f53384b1
Micah ortega <codingsynapse@gmail.com>
2019-11-15 01:13:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,6 +32,20 @@ class TicTacToe
       @board[position] = value
   end
 
+  def position_taken?(board, index)
+ if board[index] == " "
+   false
+ elsif board[index] == ""
+   false
+elsif board[index] == nil
+  false
+elsif board[index] == "X" || "O"
+  true
+end
+
+end
+
+
 
 
 
